e08ebf50ca3f7859c5dba78db079c20c8e4453b0
Tran Dong Hil <151639006+hiltd2758@users.noreply.github.com>
2026-02-19 16:21:02 +0700

    fibo

--- Fibonacci Number/leetcode509.js
+++ Fibonacci Number/leetcode509.js
@@ -19,18 +19,30 @@ console.log(Fibonacci(number));
 
 
 //space: O(1)
-function Fibonacci(number) {
+function FibonacciOptimized(number) {
     if (number === 0) return 0
     if (number === 1) return 1
 
-    let prev =0;
-    let curr=1;
+    let prev = 0;
+    let curr = 1;
 
     for (let i = 2; i <= number; i++) {
-        let next = prev + curr ;
-        
+        let next = prev + curr;
+        //next = 1 | num=2
+
+        //next = 2 | num =3
+        prev = curr
+        //prev = 1
+
+        //prev = 1
+        curr = next
+        //cur =1
+
+        //cur=2
     }
-    return ;
+
+    return curr;
 
 }
 
+console.log(FibonacciOptimized(4));
